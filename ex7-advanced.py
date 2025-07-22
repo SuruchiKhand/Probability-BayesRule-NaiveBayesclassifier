c93950e699fc1149b2e98705445198b422dab2a9
Suruchi Khand <khand.suruchi@gmail.com>
2025-07-22 16:12:35 -0400

    Advanced solution to flip the coin probability

---
+++ ex7-advanced.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def generate(p1):
+    # change this so that it generates 10000 random zeros and ones
+    # where the probability of one is p1
+    seq = np.random.choice([0,1], p=[1-p1, p1],size=10000)
+    return seq
+
+def count(seq):
+    # insert code to return the number of occurrences of 11111 in the sequence
+    s = ''.join(map(str,seq))
+    return sum(1 for i in range(len(s) - 4) if s[i:i+5] == '11111')
+
+def main(p1):
+    seq = generate(p1)
+    return count(seq)
+
+print(main(2/3))
